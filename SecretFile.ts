7128d85309fdfb956981a4bdfe0948f6f8406210
cinthia abigail <cinthiaastoraique2@gmail.com>
2024-03-10 00:07:36 -0400

    tarea realizada

---
+++ SecretFile.ts
@@ -0,0 +1,11 @@
+(() => {
+    class SecretFile {
+        secretMessage () {
+            console.log("shhhh...");
+
+        }
+    }
+    const secretFile = new SecretFile();
+    secretFile.secretMessage ();
+
+})();
